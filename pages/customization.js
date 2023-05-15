160bc9a130f02a3158729e607d3029a8dfe4b6e8
paulpenney <paul.penney@gmail.com>
2023-05-15 18:55:41 -0400

    Character Selection

--- pages/customization.js
+++ pages/customization.js
@@ -19,46 +19,60 @@ export default function customization() {
     const socket = getGlobal('socket');
 
     let characterButton1 = document.createElement('button');
-    characterButton1.id = "characterbar";
+    characterButton1.className = "characterbutton";
     characterButton1.innerHTML = "Rogue";
     characterButton1.value = "SELECT";
-    socket.emit("Character chosen", "Rogue")
+    characterButton1.onclick = function () {
+        socket.emit("Character chosen", "Rogue")
+        console.log("The Rogue aspect has been selected!");
+    };
 
     const CB1I = document.createElement('img')
     CB1I.style.width = '100px';
-    CB1I.src = 'holder.png';
+    CB1I.src = './images/rogue.png';
     characterButton1.appendChild(CB1I)
 
     const characterButton2 = document.createElement('button');
-    characterButton2.id = "characterbar";
+    characterButton2.className = "characterbutton";
     characterButton2.innerHTML = "Mage";
     characterButton2.value = "SELECT";
     socket.emit("Character chosen", "Mage")
+    characterButton2.onclick = function () {
+        socket.emit("Character chosen", "Mage")
+        console.log("The Mage aspect has been selected!");
+    };
 
     const CB2I = document.createElement('img')
     CB2I.style.width = '100px';
-    CB2I.src = 'holder.png';
+    CB2I.src = './images/mage.png';
     characterButton2.appendChild(CB2I)
 
     const characterButton3 = document.createElement('button');
-    characterButton3.id = "characterbar";
+    characterButton3.className = "characterbutton";
     characterButton3.innerHTML = "Knight";
     characterButton3.value = "SELECT";
-    socket.emit("Character chosen", "Knight")
+    characterButton3.onclick = function () {
+        socket.emit("Character chosen", "Knight")
+        console.log("The Knight aspect has been selected!");
+    };
 
     const CB3I = document.createElement('img')
-    CB3I.src = 'holder.png';
+    CB3I.src = './images/knight.png';
     CB3I.style.width = '100px';
     characterButton3.appendChild(CB3I)
 
     const characterButton4 = document.createElement('button');
-    characterButton4.id = "characterbar";
+    characterButton4.className = "characterbutton";
     characterButton4.innerHTML = "Archer";
     characterButton4.value = "SELECT";
-    socket.emit("Character chosen", "Archer")
+    characterButton4.onclick = function () {
+        socket.emit("Character chosen", "Archer")
+        // Do something when the button is clicked
+        console.log("The Archer aspect has been selected!");
+    };
 
     const CB4I = document.createElement('img')
-    CB4I.src = 'holder.png';
+    CB4I.src = './images/archer.png';
     CB4I.style.width = '100px';
     characterButton4.appendChild(CB4I)
 
@@ -80,52 +94,53 @@ export default function customization() {
     //start of each button used to select which type of element you'd like to have
 
     const elementButton1 = document.createElement('button');
-    elementButton1.id = "characterbar";
+    elementButton1.className = "characterbutton";
     elementButton1.innerHTML = "Water";
     elementButton1.value = "SELECT";
     socket.emit("Element chosen", "Water")
+    elementButton1.onclick = function () {
+        socket.emit("Element chosen", "Water")
+        console.log("The Water aspect has been selected!");
+    };
 
     const EB1I = document.createElement('img')
-    EB1I.src = 'holder.png';
+    EB1I.src = './images/water.png';
     EB1I.style.width = '100px';
     elementButton1.appendChild(EB1I)
 
     const elementButton2 = document.createElement('button');
-    elementButton2.id = "characterbar";
+    elementButton2.className = "characterbutton";
     elementButton2.innerHTML = "Fire";
     elementButton2.value = "SELECT";
     socket.emit("Element chosen", "Fire")
+    elementButton2.onclick = function () {
+        socket.emit("Element chosen", "Fire")
+        console.log("The Fire aspect has been selected!");
+    };
 
     const EB2I = document.createElement('img')
-    EB2I.src = 'holder.png';
+    EB2I.src = './images/fire.png';
     EB2I.style.width = '100px';
     elementButton2.appendChild(EB2I)
 
-    const elementButton3 = document.createElement('button');
-    elementButton3.id = "characterbar";
-    elementButton3.innerHTML = "Wind";
-    elementButton3.value = "SELECT";
-    socket.emit("Element chosen", "Wind")
-
-    const EB3I = document.createElement('img')
-    EB3I.src = 'holder.png';
-    EB3I.style.width = '100px';
-    elementButton3.appendChild(EB3I)
 
     const elementButton4 = document.createElement('button');
-    elementButton4.id = "characterbar";
+    elementButton4.className = "characterbutton";
     elementButton4.innerHTML = "Earth";
     elementButton4.value = "SELECT";
-    socket.emit("Element chosen", "Earth")
+    elementButton4.onclick = function () {
+        socket.emit("Element chosen", "Earth")
+        console.log("The Earth aspect has been selected!");
+    };
 
     const EB4I = document.createElement('img')
-    EB4I.src = 'holder.png';
+    EB4I.src = './images/earth.png';
     EB4I.style.width = '100px';
     elementButton4.appendChild(EB4I)
 
-        content.appendChild(characterBar);
+    content.appendChild(characterBar);
     content.appendChild(characterContainer)
-        content.appendChild(elementBar);
+    content.appendChild(elementBar);
     content.appendChild(elementContainer)
     characterContainer.appendChild(characterButton1)
     characterContainer.appendChild(characterButton2)
@@ -133,12 +148,11 @@ export default function customization() {
     characterContainer.appendChild(characterButton4)
     elementContainer.appendChild(elementButton1)
     elementContainer.appendChild(elementButton2)
-    elementContainer.appendChild(elementButton3)
     elementContainer.appendChild(elementButton4)
 
     const backButton = document.createElement('button');
     backButton.innerHTML = "Back";
-    backButton.onclick = function() {
+    backButton.onclick = function () {
         menu();
 
     }
