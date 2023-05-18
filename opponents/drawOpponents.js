0aa7709556def65a11c819a867adff0d5318f37d
penneytech <99091373+penneytech@users.noreply.github.com>
2023-05-18 16:02:38 +0000

    update

--- opponents/drawOpponents.js
+++ opponents/drawOpponents.js
@@ -6,48 +6,49 @@ import { getGlobal, setGlobal } from "../globals.js";
 let opponentFramecounters = {};
 
 export function drawOpponents() {
-  const ctx = getGlobal('ctx');
-  const userMap = getGlobal('userMap');
-  const canvasWidth = getGlobal('canvasWidth');
-  const canvasHeight = getGlobal('canvasHeight');
-  const playerposition = getGlobal('playerposition');
-  const characters = getGlobal('characters'); // Assuming you have the opponent images stored in the opponentImages variable
+    const ctx = getGlobal('ctx');
+    const userMap = getGlobal('userMap');
+    const canvasWidth = getGlobal('canvasWidth');
+    const canvasHeight = getGlobal('canvasHeight');
+    const playerposition = getGlobal('playerposition');
+    const characters = getGlobal('characters'); // Assuming you have the opponent images stored in the opponentImages variable
 
-  let framerate = 10;
+    let framerate = 10;
 
-  userMap.forEach((opponent, opponentId) => {
-    if (opponent.username !== getGlobal('username')) {
-      ctx.beginPath();
-      let x = opponent.x;
-      let y = opponent.y;
-      x = x - playerposition.x + canvasWidth / 2 - playerposition.width / 2;
-      y = y - playerposition.y + canvasHeight / 2 - playerposition.height / 2;
-      let opponentImage = characters[opponent.character][opponent.element];
+    userMap.forEach((opponent, opponentId) => {
+        if (opponent.username !== getGlobal('username')) {
+            ctx.beginPath();
+            let x = opponent.x;
+            let y = opponent.y;
+            x = x - playerposition.x + canvasWidth / 2 - playerposition.width / 2;
+            y = y - playerposition.y + canvasHeight / 2 - playerposition.height / 2;
+            let opponentImage = characters[opponent.character][opponent.element];
 
-      //console.log("IMAGE", opponentImage, opponent.character, opponent.element)
-      // Initialize frame counter for opponent if not already present
-      if (!opponentFramecounters[opponentId]) {
-        opponentFramecounters[opponentId] = 0;
-      }
+            //console.log("IMAGE", opponentImage, opponent.character, opponent.element)
+            // Initialize frame counter for opponent if not already present
+            if (!opponentFramecounters[opponentId]) {
+                opponentFramecounters[opponentId] = 0;
+            }
 
-      // Determine which opponent image to load based on frame count
-      const opponentImageIndex = Math.floor(opponentFramecounters[opponentId] / framerate);
-      if (opponentImage[opponentImageIndex]) {
-        ctx.drawImage(opponentImage[opponentImageIndex], x, y, 50, 50);
-      }
+            // Determine which opponent image to load based on frame count
 
-      // Render opponent name
-      ctx.font = '14px Arial';
-      ctx.textAlign = 'center';
-      ctx.fillStyle = 'white';
-      ctx.fillText(opponent.username, x + 25, y - 10);
+            const opponentImageIndex = Math.floor(opponentFramecounters[opponentId] / framerate);
+            if (opponentImage[opponentImageIndex]) {
+                ctx.drawImage(opponentImage[opponentImageIndex], x, y, 50, 50);
+            }
 
-      // Increase frame counter
-      opponentFramecounters[opponentId]++;
+            // Render opponent name
+            ctx.font = '14px Arial';
+            ctx.textAlign = 'center';
+            ctx.fillStyle = 'white';
+            ctx.fillText(opponent.username, x + 25, y - 10);
 
-      if (opponentFramecounters[opponentId] > framerate * 3) {
-        opponentFramecounters[opponentId] = 0;
-      }
-    }
-  });
+            // Increase frame counter
+            opponentFramecounters[opponentId]++;
+
+            if (opponentFramecounters[opponentId] > framerate * 3) {
+                opponentFramecounters[opponentId] = 0;
+            }
+        }
+    });
 }
